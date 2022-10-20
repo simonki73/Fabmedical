564ce6ee93934a6928273454934c7e414e6ed9c5
simonki73 <simon.kirkley@quilter.com>
2022-10-20 11:39:13 +0000

    Initial Commit

--- scripts/create_build_environment.sh
+++ scripts/create_build_environment.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -#!/bin/bash
 
 # Check if SUFFIX envvar exists
 if [[ -z "${MCW_SUFFIX}" ]]; then
@@ -32,7 +32,7 @@ sudo apt install -y \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get install curl python-software-properties -y
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get update -y && sudo apt-get install -y docker-ce nodejs mongodb-clients
 sudo apt-get upgrade -y
 
